89a9416e898d050449734f06c3e43561cf9ee0f2
Geoffrey Thompson <gzthompson@gmail.com>
2018-02-02 17:21:46 -0600

    Added more comments and error checking.

--- matrixnorm.R
+++ matrixnorm.R
@@ -93,6 +93,9 @@ mle.matrixnorm = function(data,row.restrict="none",col.restrict="none",tol = 1e-
   # if list, presumes is a list of the matrices
   # will start by working with assumption that data is an array
   dims = dim(data)
+  
+  if(max(dims[2]/dims[3],dims[3]/dims[2])<(dims[1]-1)) warning("Need more observations to estimate parameters.")
+  # don't have initial starting point for U and V, start with diag.
   if(missing(U)) U = diag(dims[2])
   if(missing(V)) V = diag(dims[3])
   mu = apply(data,c(2,3),mean)
@@ -107,7 +110,8 @@ mle.matrixnorm = function(data,row.restrict="none",col.restrict="none",tol = 1e-
     
     inter.U =  apply(swept.data, 1, function(x) ((x) %*% solve(V) %*% t(x)) )
     new.U = matrix(apply(inter.U ,1,sum),nrow=dims[2]) / (dims[1]*dims[3])
-    new.U = new.U/(new.U[1,1]) # only identifiable up to a constant, so have to fix at 1
+    new.U = new.U/(new.U[1,1]) # only identifiable up to a constant, so have to fix something at 1
+    # compute differences with prior iterations:
     error.term = max(sum((new.V - V)^2), sum((new.U - U)^2))
     V = new.V
     U = new.U
