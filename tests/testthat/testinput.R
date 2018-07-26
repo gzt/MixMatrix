2e65131145a8957aa882c67e41ec2d85fbbafda9
gzt <gzthompson@gmail.com>
2018-07-26 18:13:01 -0500

    add input validation tests for mlmatrixt

--- tests/testthat/testinput.R
+++ tests/testthat/testinput.R
@@ -147,6 +147,19 @@ test_that("trying wrong type of input", {
                             V = matrix("Q", nrow = 5, ncol = 5)),
                "non-numeric", ignore.case = TRUE)
 
+  expect_error(MLmatrixt(rmatrixnorm(n = 100, mean = diag(5)), tol =
+                              "Q"), "non-numeric", ignore.case = TRUE)
+  expect_error(MLmatrixt(rmatrixnorm(n = 100, mean = diag(5)), max.iter =
+                              "Q"), "non-numeric", ignore.case = TRUE)
+  expect_error(MLmatrixt(rmatrixnorm(n = 100, mean = diag(5)), df =
+                           "Q"), "non-numeric", ignore.case = TRUE)
+  expect_error(MLmatrixt(rmatrixnorm(n = 100, mean = diag(5)),
+                            U = matrix("Q", nrow = 5, ncol = 5)),
+               "non-numeric", ignore.case = TRUE)
+  expect_error(MLmatrixt(rmatrixnorm(n = 100, mean = diag(5)),
+                            V = matrix("Q", nrow = 5, ncol = 5)),
+               "non-numeric", ignore.case = TRUE)
+
   # expect_error(toepgenerate(7, "A"))
   # expect_error(toepgenerate("A", .4))
 
@@ -436,7 +449,8 @@ test_that("Testing bad matrix dimension input", {
 
   expect_error(MLmatrixnorm(rmatrixnorm(n = 100, mean = A), U = B, V = A))
   expect_error(MLmatrixnorm(rmatrixnorm(n = 100, mean = A), U = A, V = B))
-
+  expect_error(MLmatrixt(rmatrixnorm(n = 100, mean = A), U = B, V = A))
+  expect_error(MLmatrixt(rmatrixnorm(n = 100, mean = A), U = A, V = B))
 
 })
 
