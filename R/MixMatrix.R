637ac7c929258e2a22d339e48f0b2f572bd61cda
gzt <gzthompson@gmail.com>
2019-06-16 19:12:51 -0500

    regenerating files for mixmat

--- R/MixMatrix.R
+++ R/MixMatrix.R
@@ -1,6 +1,6 @@
 #' Functions for Matrix Distributions
 #'
-#' The matrixdist package current provides for generating
+#' The MixMatrix package current provides for generating
 #' matrix variate random variables, computing matrix variate densities,
 #' and fitting the MLEs for parameters of matrix variate normal distributions.
 #' Currently, normal, t, and inverted t distributions are supported for generation
