540c0e575d41406a0112fd4473301d34a92eb298
gzt <gzthompson@gmail.com>
2019-06-22 13:35:36 -0500

    add license notes to all files

--- R/matnormlda.R
+++ R/matnormlda.R
@@ -1,3 +1,29 @@
+#   matnormlda.R
+#   MixMatrix: Classification with Matrix Variate Normal and t distributions
+#   Copyright (C) 2018-9  GZ Thompson <gzthompson@gmail.com>
+#
+#   These functions are based on modifications of the source for 
+#   MASS::lda() and MASS::qda(),
+#   copyright (C) 1994-2013 W. N. Venables and B. D. Ripley
+#   released under GPL 2 or greater. This software is released under GPL-3.
+#
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#     You should have received a copy of the GNU General Public License
+#   along with this program; if not, a copy is available at
+#   https://www.R-project.org/Licenses/
+
+
+
+
 #' LDA for matrix variate distributions
 #'
 #' Performs linear discriminant analysis on matrix variate data.
