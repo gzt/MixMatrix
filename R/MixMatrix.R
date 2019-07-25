418116852a54bf8dbc926a02dd72b202da71ca70
gzt <gzthompson@gmail.com>
2019-07-24 21:59:53 -0500

    one last bit of modification to description and documentation

--- R/MixMatrix.R
+++ R/MixMatrix.R
@@ -1,11 +1,12 @@
 #' Functions for Matrix Distributions
 #'
 #' Provides sampling and density functions for matrix
-#' variate normal, t, and inverted t distributions;  ML fitting for matrix
+#' variate normal, t, and inverted t distributions;  ML estimation for matrix
 #' variate normal and t distributions including some restrictions on the
-#' parameters; and linear and quadratic discriminant analysis for matrix
-#' variate normal and t distributions. Performs mixture modeling for matrix
-#' variate distributions.
+#' parameters using the EM algorithm; and classification by linear and quadratic discriminant
+#' analysis for matrix variate normal and t distributions described
+#' in \href{https://arxiv.org/abs/1907.09565}{Thompson et al. (2019)}.
+#' Performs clustering with matrix variate normal and t mixture models.
 #'
 #' @docType package
 #' @name MixMatrix
