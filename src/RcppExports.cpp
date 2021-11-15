d33519d74975c0a3fcaf4fb4fea2d0a52a335627
gzt <gzthompson@gmail.com>
2021-11-14 19:39:26 -0500

    minor updates to fix URLs and other minor changes

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -6,6 +6,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // posdefsqrt
 arma::mat posdefsqrt(arma::mat& x);
 RcppExport SEXP _MixMatrix_posdefsqrt(SEXP xSEXP) {
